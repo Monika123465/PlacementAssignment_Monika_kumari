0ffda0c9ba724da880197b40fdff121b7c8b8cc6
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-06-02 21:43:50 +0530

    front added

--- Ecommerce/client/src/App.js
+++ Ecommerce/client/src/App.js
@@ -1,13 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Route,
+  
+} from "react-router-dom";
+import { redirect } from "react-router-dom";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
+
+// ab is tarike se mat karo ab is tarah se karo 
+
+const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
   return (
-    <div >
-      
-      
-    </div>
+    <Router>
+        <Route exact path="/" element={<Home/>} >
+        
+        </Route>
+        <Route path="/products/:category" element={<ProductList />}>
+          
+        </Route>
+        <Route path="/product/:id" element={ <Product />}>
+          </Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/success" element={ <Success />}>
+         
+        </Route>
+        <Route path="/login">{user ? <redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <redirect to="/" />: <Register />}
+        </Route>
+    </Router>
   );
-}
+};
 
 export default App;
